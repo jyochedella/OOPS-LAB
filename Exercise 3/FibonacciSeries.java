5f928423b7d7391beb773998792d9aca7539cdc5
jyochedella <jyochedella@gmail.com>
2025-03-12 18:55:35 +0530

    Add files via upload

---
+++ Exercise 3/FibonacciSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        
+        int a = 0, b = 1, next;
+        System.out.print("Fibonacci Series: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+
+        sc.close();
+    }
+}
